9b1370cb769d4ad77e423571557ae6820e405915
SkippsDev <SkippsDev@users.noreply.github.com>
2016-08-15 16:21:45 -0400

    Fixed scheduleReport method in DistributedReportMgrAI

--- toontown/ai/DistributedReportMgrAI.py
+++ toontown/ai/DistributedReportMgrAI.py
@@ -3,12 +3,6 @@ from direct.distributed.DistributedObjectAI import DistributedObjectAI
 from toontown.uberdog.ClientServicesManagerUD import executeHttpRequestAndLog
 import ReportGlobals, threading, time
 
-# TODO: FIX
-
-'''
-THREADING.TIMER CAUSES CONTROL C NOT TO WORK, AND FOR THE AI NOT TO DIE
-'''
-
 class DistributedReportMgrAI(DistributedObjectAI):
     notify = DirectNotifyGlobal.directNotify.newCategory("DistributedReportMgrAI")
 
@@ -16,10 +10,16 @@ class DistributedReportMgrAI(DistributedObjectAI):
         DistributedObjectAI.__init__(self, air)
         self.reports = []
         self.interval = config.GetInt('report-interval', 600)
-        #self.scheduleReport()
+        taskMgr.add('schedule-report', self.scheduleReport())
 
-    #def scheduleReport(self):
-    #    threading.Timer(self.interval, self.sendAllReports).start()
+    def scheduleReport(self, task):
+        self.sendAllReports()
+        
+        # add a delay to the task.
+        task.setDelay(self.interval)
+        
+        # loop the task.
+        return task.again
 
     def sendReport(self, avId, category):
         if not ReportGlobals.isValidCategoryName(category) or not len(str(avId)) == 9:
@@ -34,8 +34,6 @@ class DistributedReportMgrAI(DistributedObjectAI):
         self.reports.append('%s|%s|%s|%s' % (timestamp, reporter.doId, avId, category))
 
     def sendAllReports(self):
-        #self.scheduleReport()
-
         if not self.reports or config.GetString('accountdb-type', 'developer') != 'remote':
             return
 
