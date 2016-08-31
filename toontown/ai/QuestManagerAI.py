5d36aad4171846db0d36f90768487762b806b962
ToontownModder101 <jareddarty96@gmail.com>
2016-08-31 05:42:23 -0400

    Revert "Revert "Fixed AI crash in QuestManagerAI""
    This reverts commit c4db760d41f2da3cea093b3409ead0a929d70e4d.

--- toontown/ai/QuestManagerAI.py
+++ toontown/ai/QuestManagerAI.py
@@ -514,7 +514,7 @@ class QuestManagerAI:
             if isinstance(questClass, Quests.CogQuest):
                 # Check if the cog counts...
                 for suit in suitsKilled:
-                    if questClass.doesCogCount(av.doId, suit, zoneId):
+                    if questClass.doesCogCount(av.doId, suit, zoneId, avList=[]):
                         # Looks like the cog counts!
                         if questClass.getCompletionStatus(av, questDesc) != Quests.COMPLETE:
                             questDesc[QuestProgressIndex] += 1
