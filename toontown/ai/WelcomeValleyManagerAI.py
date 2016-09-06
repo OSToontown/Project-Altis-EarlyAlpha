3b730f973a71c709c5d43d9c7466d34c0927e9e4
malverde <malverdedeyum@hotmail.com>
2016-09-07 00:38:02 +0100

    Fixes crash

--- toontown/ai/WelcomeValleyManagerAI.py
+++ toontown/ai/WelcomeValleyManagerAI.py
@@ -7,9 +7,11 @@ class WelcomeValleyManagerAI(DistributedObjectAI):
     def clientSetZone(self, todo0):
         pass
 
+    def toonSetZone(self, doId, newZoneId):
+        pass
+
     def requestZoneIdMessage(self, todo0, todo1):
         pass
 
     def requestZoneIdResponse(self, todo0, todo1):
         pass
-
