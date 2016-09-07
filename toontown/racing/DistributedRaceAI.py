d4542b0ca4570df911b7fdb89fd3f881be6f1e87
malverde <malverdedeyum@hotmail.com>
2016-09-07 01:32:02 +0100

    Racing fixes

--- toontown/racing/DistributedRaceAI.py
+++ toontown/racing/DistributedRaceAI.py
@@ -13,7 +13,7 @@ import random
 
 class DistributedRaceAI(DistributedObjectAI, FSM):
     notify = DirectNotifyGlobal.directNotify.newCategory("DistributedRaceAI")
-    
+
     def __init__(self, air):
         DistributedObjectAI.__init__(self, air)
         FSM.__init__(self, 'DistributedRaceAI')
@@ -32,12 +32,12 @@ class DistributedRaceAI(DistributedObjectAI, FSM):
         self.livingGags = []
         self.currentlyAffectedByAnvil = {}
         self.avatarProgress = {}
-    
+
     def generate(self):
         for avatar in self.avatars:
             self.acceptOnce(self.air.getAvatarExitEvent(avatar), self.playerLeave, [avatar])
         self.demand('Join')
-        
+
     def delete(self):
         for aK in self.avatarKarts:
             kart = self.air.doId2do[aK[1]]
@@ -45,49 +45,49 @@ class DistributedRaceAI(DistributedObjectAI, FSM):
         for gag in self.livingGags:
             gag.requestDelete()
         self.air.deallocateZone(self.zoneId)
-        for i in range(len(self.gags)):
+        for i in xrange(len(self.gags)):
             taskMgr.remove('regenGag%i-%i' % (i, self.doId))
         DistributedObjectAI.delete(self)
-        
+
     def enterJoin(self):
         self.beginBarrier('waitingForJoin', self.avatars, 60, self.joinBarrierCallback)
         self.d_waitingForJoin()
-        
+
     def exitJoin(self):
         pass
-        
+
     def enterPrep(self):
         self.beginBarrier('waitingForReady', self.avatars, 60, self.readyBarrierCallback)
         self.gagPoints = RaceGlobals.TrackDict[self.trackId][4]
         if self.raceType != RaceGlobals.Practice:
-            for i in range(len(self.gagPoints)):
+            for i in xrange(len(self.gagPoints)):
                 gagId = random.randint(0, 5)
                 self.b_genGag(i, 1, gagId)
         self.d_prepForRace()
-        
+
     def exitPrep(self):
         pass
-        
+
     def enterTutorial(self):
         self.beginBarrier('readRules', self.avatars, 30, self.readRulesCallback)
         self.d_startTutorial()
-    
+
     def exitTutorial(self):
         pass
-        
+
     def enterStart(self):
         self.startTime = globalClockDelta.networkToLocalTime(globalClockDelta.getRealNetworkTime()) + 3
         self.b_startRace(3)
-    
+
     def exitStart(self):
         pass
-    
+
     def readyBarrierCallback(self, avatars):
         self.demand('Tutorial')
-        
+
     def readRulesCallback(self, avatars):
         self.demand('Start')
-    
+
     def joinBarrierCallback(self, avatars):
         for av in self.avatars:
             if not av in avatars:
@@ -98,123 +98,121 @@ class DistributedRaceAI(DistributedObjectAI, FSM):
             self.avatarKarts.append([av, kart.getDoId()])
         self.beginBarrier('waitingForPrep', self.avatars, 60, self.prepBarrierCallback)
         self.sendUpdate('setEnteredRacers', [self.avatarKarts])
-        
+
     def prepBarrierCallback(self, avatars):
         self.demand('Prep')
 
-    
     def setZoneId(self, zoneId):
         self.zoneId = zoneId
-        
+
     def d_setZoneId(self, zoneId):
         self.sendUpdate('setZoneId', [zoneId])
-    
+
     def b_setZoneId(self, zoneId):
         self.setZoneId(zoneId)
         self.d_setZoneId(zoneId)
-    
+
     def getZoneId(self):
         return self.zoneId
 
     def setTrackId(self, trackId):
         self.trackId = trackId
-        
+
     def getTrackId(self):
         return self.trackId
 
     def setRaceType(self, raceType):
         self.raceType = raceType
-    
+
     def getRaceType(self):
         return self.raceType
-    
+
     def setCircuitLoop(self, circuitLoop):
         self.circuitLoop = circuitLoop
-        
+
     def getCircuitLoop(self):
         return self.circuitLoop
 
     def setAvatars(self, avatarList):
         self.avatars = avatarList
-        
+
     def getAvatars(self):
         return self.avatars
 
     def setStartingPlaces(self, startingPlaces):
         self.startingPlaces = startingPlaces
-        
+
     def getStartingPlaces(self):
         return self.startingPlaces
 
     def setLapCount(self, lapCount):
         self.lapCount = lapCount
-        
+
     def getLapCount(self):
         return self.lapCount
 
     def waitingForJoin(self):
         self.beginBarrier('waitingForJoin', self.avatars, 60, self.b_prepForRace)
-    
+
     def d_waitingForJoin(self):
         self.sendUpdate('waitingForJoin', [])
-        
+
     def b_waitingForJoin(self):
         self.waitingForJoin()
         self.d_waitingForJoin()
 
     def setEnteredRacers(self, todo0):
         pass
-    
-    
+
     def d_prepForRace(self):
         self.sendUpdate('prepForRace', [])
-    
+
     def b_prepForRace(self, avatars):
         self.prepForRace()
         self.d_prepForRace()
-    
+
     def startTutorial(self):
         self.beginBarrier('readRules', self.avatars, 60, self.raceStart)
-    
+
     def d_startTutorial(self):
         self.sendUpdate('startTutorial', [])
-    
+
     def b_startTutorial(self, avatars):
         self.startTutorial()
         self.d_startTutorial()
 
     def startRace(self, timeUntilStart):
         taskMgr.doMethodLater(timeUntilStart, self.startKarts, 'startKarts%i' % self.doId, [])
-        
+
     def startKarts(self):
         for avatarKart in self.avatarKarts:
-            kart = self.air.doId2do[avatarKart[1]]
-            kart.sendUpdate('setInput', [1])
-            self.avatarProgress[avatarKart[0]] = 0
-            self.avatarGags[avatarKart[0]] = 0
-            self.currentlyAffectedByAnvil[avatarKart[0]]  = False
-
+            if avatarKart[1] in self.air.doId2do:
+                kart = self.air.doId2do[avatarKart[1]]
+                kart.sendUpdate('setInput', [1])
+                self.avatarProgress[avatarKart[0]] = 0
+                self.avatarGags[avatarKart[0]] = 0
+                self.currentlyAffectedByAnvil[avatarKart[0]]  = False
 
     def b_startRace(self, timeUntilStart):
         self.startRace(timeUntilStart)
         self.d_startRace(timeUntilStart)
-    
+
     def d_startRace(self, timeUntilStart):
         self.sendUpdate('startRace', [globalClockDelta.localToNetworkTime(globalClockDelta.globalClock.getRealTime() + timeUntilStart)])
-    
+
     def goToSpeedway(self, todo0, todo1):
         pass
 
     def genGag(self, slot, number, type):
         self.gags[slot] = [number, type]
-        
+
     def d_genGag(self, slot, number, type):
         self.sendUpdate('genGag', [slot, number, type])
-    
+
     def b_genGag(self, slot, number, type):
         self.genGag(slot, number, type)
         self.d_genGag(slot, number, type)
-    
+
     def dropAnvilOn(self, todo0, todo1, todo2):
         pass
 
@@ -320,13 +318,12 @@ class DistributedRaceAI(DistributedObjectAI, FSM):
             av.b_setTickets(RaceGlobals.MaxTickets)
         self.sendUpdate('setPlace', [avId, totalTime, place, entryFee, qualify, (winnings-entryFee), bonus, trophies, [], 0])
 
-        
     def calculateTrophies(self, avId, won, qualify, time):
         av = self.air.doId2do[avId]
         kartingHistory = av.getKartingHistory()
         avTrophies = av.getKartingTrophies()
         numTrophies = 0
-        for i in range(30):
+        for i in xrange(30):
             if avTrophies[i] != 0:
                 numTrophies += 1
         oldLaffBoost = int(numTrophies/10)
@@ -338,7 +335,7 @@ class DistributedRaceAI(DistributedObjectAI, FSM):
             if kartingHistory[3] > RaceGlobals.TotalWonRaces:
                 avTrophies[RaceGlobals.TotalWins] = 1
                 trophies.append(RaceGlobals.TotalWins)
-            for i in range(3):
+            for i in xrange(3):
                 if kartingHistory[genre] >= RaceGlobals.WonRaces[i] and avTrophies[RaceGlobals.AllWinsList[genre][i]] != 1:
                     avTrophies[RaceGlobals.AllWinsList[genre][i]] = 1
                     trophies.append(RaceGlobals.AllWinsList[genre][i])
@@ -348,10 +345,13 @@ class DistributedRaceAI(DistributedObjectAI, FSM):
             if kartingHistory[7] >= RaceGlobals.TotalQualifiedRaces and avTrophies[RaceGlobals.TotalQuals] != 1:
                 avTrophies[RaceGlobals.TotalQuals] = 1
                 trophies.append(RaceGlobals.TotalQuals)
-            for i in range(3):
+            for i in xrange(3):
                 if kartingHistory[genre + 4] >= RaceGlobals.QualifiedRaces[i] and avTrophies[RaceGlobals.AllQualsList[genre][i]] != 1:
                     avTrophies[RaceGlobals.AllQualsList[genre][i]] = 1
                     trophies.append(RaceGlobals.AllQualsList[genre][i])
+        for i, history in enumerate(kartingHistory):
+            if history > 255:
+                kartingHistory[i] = 255
         av.b_setKartingHistory(kartingHistory)
         pKartingBest = av.getKartingPersonalBestAll()
         trackIndex = TTLocalizer.KartRace_TrackNames.keys().index(self.trackId)
@@ -368,7 +368,7 @@ class DistributedRaceAI(DistributedObjectAI, FSM):
                 trophies.append(RaceGlobals.GrandTouring)
         newLaffBoost = int((len(trophies) + numTrophies)/10)
         if newLaffBoost - oldLaffBoost != 0:
-            for i in range(newLaffBoost):
+            for i in xrange(newLaffBoost):
                 if avTrophies[RaceGlobals.TrophyCups[i]] != 1:
                     avTrophies[RaceGlobals.TrophyCups[i]] = 1
                     trophies.append(RaceGlobals.TrophyCups[i])
@@ -413,10 +413,10 @@ class DistributedRaceAI(DistributedObjectAI, FSM):
         else:
             self.air.writeServerEvent('suspicious', avId=avId, issue='Toon use race gag while not having one!')
         self.avatarGags[avId] = 0
-    
+
     def unsquish(self, avId):
         self.currentlyAffectedByAnvil[avId] = False
-        
+
     def playerLeave(self, avId):
         self.sendUpdate('racerDisconnected', [avId])
         if avId in self.avatars:
@@ -440,4 +440,3 @@ class DistributedRaceAI(DistributedObjectAI, FSM):
             if aK[0] == avId:
                 self.air.doId2do[aK[1]].request('Controlled', avId)
                 self.air.doId2do[aK[1]].sendUpdate('setInput', [0])
-
