f7b55ad0f1a075f50bcb175062c74522dfa3953b
malverde <malverdedeyum@hotmail.com>
2016-09-08 23:54:58 +0100

    Part 3

--- DEV_Launcher.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-title Project Altis
-
-"C:\Panda3D-1.9.0\python\ppython.exe" -m toontown.launcher.DEV_Launcher
--- toontown/cogdominium/DistributedCogdoElevatorIntAI.py
+++
@@ -1,5 +0,0 @@
-from direct.directnotify.DirectNotifyGlobal import directNotify
-from toontown.building.DistributedElevatorIntAI import DistributedElevatorIntAI
-
-class DistributedCogdoElevatorIntAI(DistributedElevatorIntAI):
-    notify = directNotify.newCategory('DistributedCogdoElevatorIntAI')
