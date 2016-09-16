a78394189ab42833ab8f7376d44bd35919fe7d84
ToontownModder101 <jareddarty96@gmail.com>
2016-09-16 06:08:30 -0400

    Texture Update
    1. Plus Bossbot textures are normal again (still HD, just no longer
    darker).
    2. Song and Dance new sound reverted.
    3. More TT 2.0 textures.

--- toontown/battle/MovieSuitAttacks.py
+++ toontown/battle/MovieSuitAttacks.py
@@ -857,7 +857,7 @@ def doSongAndDance(attack):
     battle = attack['battle']
     suitTrack = getSuitTrack(attack)
     toonTrack = getToonTrack(attack, suitTrack.getDuration() - 1.8, ['cringe'], suitTrack.getDuration() - 1.8, ['applause'])
-    soundTrack = getSoundTrack('SA_song_and_dance.ogg', node=suit)
+    soundTrack = getSoundTrack('AA_heal_happydance.ogg', node=suit)
     return Parallel(suitTrack, toonTrack, soundTrack)
 
 def doFillWithLead(attack):
