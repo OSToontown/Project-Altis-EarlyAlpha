920568a48214ab7b163ac4e6d76a1e2138d4df56
ToontownModder101 <jareddarty96@gmail.com>
2016-09-18 01:18:59 -0400

    Small Bossbot HQ tweak

--- toontown/coghq/DistributedCountryClub.py
+++ toontown/coghq/DistributedCountryClub.py
@@ -49,7 +49,7 @@ class DistributedCountryClub(DistributedObject.DistributedObject):
         return
 
     def startSky(self):
-        self.sky = loader.loadModel('phase_12/models/bossbotHQ/BossTestSkyBox')
+        self.sky = loader.loadModel('phase_9/models/cogHQ/cog_sky')
         self.sky.reparentTo(camera)
         self.sky.setZ(0.0)
         self.sky.setHpr(0.0, 0.0, 0.0)
@@ -65,7 +65,7 @@ class DistributedCountryClub(DistributedObject.DistributedObject):
         self.startSky()
         sky = self.sky
         sky.setH(150)
-        sky.setZ(-100)
+        sky.setZ(-150)
 
     def __cleanupHighSky(self):
         sky = self.sky
