8e1ecc28917b082f41dc02e6f006e2c980540e4f
Mango-Monkey <mangomonkey47@gmail.com>
2016-09-25 13:20:44 -0400

    Testing Something

--- toontown/toonbase/ToontownGlobals.py
+++ toontown/toonbase/ToontownGlobals.py
@@ -106,12 +106,14 @@ MaxHpLimit = 151
 MaxCarryLimit = 80
 MaxQuestCarryLimit = 4
 GravityValue = 32.174
-MaxCogSuitLevel = 51 - 1
+MaxCogSuitLevel = 101 - 1
 CogSuitHPLevels = (15 - 1,
  20 - 1,
  30 - 1,
  40 - 1,
- 51 - 1)
+ 50 - 1,
+ 75 - 1,
+ 100 - 1)                  
 setInterfaceFont(TTLocalizer.InterfaceFont)
 setSignFont(TTLocalizer.SignFont)
 from toontown.toontowngui import TTDialog
