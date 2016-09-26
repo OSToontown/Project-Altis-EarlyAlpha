c2af4a34997e150a49ba594ca8b5e98bc93bb8ef
Spiderlover <darthvader438@gmail.com>
2016-09-25 22:51:01 -0400

    Fixed error with fireworks

--- toontown/hood/SellbotHQAI.py
+++ toontown/hood/SellbotHQAI.py
@@ -9,6 +9,7 @@ from toontown.toonbase import ToontownGlobals
 
 
 class SellbotHQAI(CogHQAI.CogHQAI):
+    HOOD = ToontownGlobals.SellbotHQ
     def __init__(self, air):
         CogHQAI.CogHQAI.__init__(
             self, air, ToontownGlobals.SellbotHQ, ToontownGlobals.SellbotLobby,
@@ -38,7 +39,7 @@ class SellbotHQAI(CogHQAI.CogHQAI):
 
         # Our suit planner needs the Cog HQ doors as well:
         for sp in self.suitPlanners:
-            if sp.zoneId == self.zoneId:
+            if sp.zoneId == self.HOOD:
                 sp.cogHQDoors = self.cogHQDoors
 
     def createFactoryElevators(self):
