d608c74064ac42488cb590bbefad5a1f609561d1
Spiderlover <darthvader438@gmail.com>
2016-09-25 21:36:05 -0400

    Source revert

--- DEV_Launcher.bat
+++ DEV_Launcher.bat
@@ -4,5 +4,5 @@ title Project Altis
 
 set /P PPYTHON_PATH=<PPYTHON_PATH
 
-"C:\Panda3D-1.10.0\python\ppython.exe" -m toontown.launcher.DEV_Launcher
+"C:\Panda3D-1.9.0\python\ppython.exe" -m toontown.launcher.DEV_Launcher
 pause
