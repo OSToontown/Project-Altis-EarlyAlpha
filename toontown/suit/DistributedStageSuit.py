f9f31f6cffc660666d241f5cd0e49e0e418e55c7
CheezedFish <watdou606@gmail.com>
2016-10-29 09:33:28 -0400

    Hotfix for facility bosses with revives
    Fixed a simple mistake during the porting process

--- toontown/suit/DistributedStageSuit.py
+++ toontown/suit/DistributedStageSuit.py
@@ -35,7 +35,7 @@ class DistributedStageSuit(DistributedFactorySuit.DistributedFactorySuit):
         if self.getSkeleRevives() > 0:
             nameInfo = TTLocalizer.SuitBaseNameWithLevel % {'name': TTLocalizer.DistrictAttorney,
              'dept': self.getStyleDept(),
-             'level': '%s%s' % (self.getActualLevel(), TTLocalizer.SkeleRevivePostFix % (self.skeleRevives + 1))}
+             'level': '%s%s' % (self.getActualLevel(), TTLocalizer.SkeleRevivePostFix)}
             self.setName(TTLocalizer.DistrictAttorney)
             self.setDisplayName(nameInfo)
         else:
