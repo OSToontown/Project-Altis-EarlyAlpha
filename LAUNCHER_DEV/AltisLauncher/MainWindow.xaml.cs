fd3911a7b581784abf09d8e5419034d8bfe369d4
Drew <drewgamesaw@gmail.com>
2016-11-05 22:17:37 -0400

    Launcher Developing files

---
+++ LAUNCHER_DEV/AltisLauncher/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AltisLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void playGame(object sender, RoutedEventArgs e)
+        {
+            Process.Start("launchAltis.bat", user.Text + " " + passwordBox.Password);
+            // TODO: Connect properly to the login server and don't just use a bat file
+            Environment.Exit(0);
+        }
+
+    }
+}
