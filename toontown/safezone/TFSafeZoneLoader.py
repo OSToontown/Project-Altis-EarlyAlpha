f2c38ef0016a9d875f36a867bf0f8952d6d7910e
ToontownModder101 <jareddarty96@gmail.com>
2016-11-29 15:48:15 -0500

    Re-add and Rename Dev Area after it was accidently removed.

--- toontown/safezone/TFSafeZoneLoader.py
+++ toontown/safezone/TFSafeZoneLoader.py
@@ -11,8 +11,8 @@ class TFSafeZoneLoader(SafeZoneLoader):
     def __init__(self, hood, parentFSM, doneEvent):
         SafeZoneLoader.__init__(self, hood, parentFSM, doneEvent)
         self.playgroundClass = Playground
-        self.musicFile = 'phase_6/audio/bgm/Dev_Land.ogg'
-        self.activityMusicFile = 'phase_6/audio/bgm/Dev_Land.ogg' 
+        self.musicFile = 'phase_6/audio/bgm/TZ_nbrhood.ogg'
+        self.activityMusicFile = 'phase_6/audio/bgm/TZ_nbrhood.ogg' 
         self.dnaFile = 'phase_6/dna/toonfest_sz.xml'
         self.safeZoneStorageDNAFile = 'phase_6/dna/storage_TF.xml'
 
