f4f7de99fab2940f18467d6188c8566847c2f6b1
Drew <drewgamesaw@gmail.com>
2016-12-03 22:26:40 -0500

    Revert removing graphicsengine render frame. it wasnt working because panda wasnt using openGL :'(

--- toontown/toonbase/ToontownStart.py
+++ toontown/toonbase/ToontownStart.py
@@ -62,8 +62,9 @@ loadPrcFileData('Settings: res', 'win-size %d %d' % tuple(settings['res']))
 loadPrcFileData('Settings: fullscreen', 'fullscreen %s' % settings['fullscreen'])
 loadPrcFileData('Settings: musicVol', 'audio-master-music-volume %s' % settings['musicVol'])
 loadPrcFileData('Settings: sfxVol', 'audio-master-sfx-volume %s' % settings['sfxVol'])
-loadPrcFileData('Settings: loadDisplay', 'load-display %s' % settings['loadDisplay'])
+#loadPrcFileData('Settings: loadDisplay', 'load-display %s' % settings['loadDisplay'])
 loadPrcFileData('Settings: newGui', 'newGui %s' % settings['newGui'])
+loadPrcFileData('', 'load-display pandagl')
 
 class game:
     name = 'toontown'
