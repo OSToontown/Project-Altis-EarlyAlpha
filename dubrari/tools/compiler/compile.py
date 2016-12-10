db4b0b5cb5412333c31fe63c10d86ab9adc14fee
Drew <drewgamesaw@gmail.com>
2016-12-09 19:30:29 -0500

    Update PDNA Compiler

--- dubrari/tools/compiler/compile.py
+++ dubrari/tools/compiler/compile.py
@@ -1,27 +1,25 @@
+#!/usr/bin/env python2
+from ply import lex
+from dna.base import DNAStorage
+from dna.components import DNARoot
+from dna.parser.tokens import *
+
 import argparse
 import os
 import sys
-
+import glob
 
 parser = argparse.ArgumentParser(
-    description='This script can be used to produce compiled DNA files.')
-parser.add_argument('--output', '-o',
-                    help='The compiled output file. Default: FILENAME.pdna')
+    description='This utility can be used to produce compiled DNA files.')
 parser.add_argument('--compress', '-c', action='store_true',
                     help='Compress the output file using ZLib.')
 parser.add_argument('--verbose', '-v', action='store_true',
                     help='Describe the build process.')
 parser.add_argument('--logfile', '-l',
                     help='Optional file to write the console output to.')
-parser.add_argument('filename',
-                    help='The raw input file.')
+parser.add_argument('filenames', nargs='+',
+                    help='The raw input file(s). Accepts * and ? as wildcards.')
 args = parser.parse_args()
-
-if not args.output:
-    args.output = os.path.splitext(args.filename)[0] + '.pdna'
-
-print 'Output:', args.output
-
 
 class LogAndOutput:
     def __init__(self, out, filename):
@@ -42,9 +40,6 @@ if args.logfile:
     sys.stdout = LogAndOutput(sys.stdout, args.logfile)
     sys.stderr = LogAndOutput(sys.stderr, args.logfile)
 
-import ply.lex as lex
-from tokens import *
-
 
 lexer = lex.lex(optimize=0)
 
@@ -54,58 +49,60 @@ class DNAError(Exception):
 __builtins__.DNAError = DNAError
 
 
-import struct
-
-from components import DNAData
-import DNAStorage
-
-
 def loadDNAFile(dnaStore, filename):
     print 'Reading DNA file...', filename
-    dnaData = DNAData.DNAData('pdna_compiler')
-    dnaData.setDnaStorage(dnaStore)
-    dnaData.read(open(filename, 'r'))
-    packedData = dnaData.traverse(recursive=True, verbose=args.verbose)
-    return packedData
+    root = DNARoot.DNARoot(name='root', dnaStore=dnaStore)
+    with open(filename, 'r') as f:
+        data = f.read().strip()
+        if not data:
+            print 'Warning', filename, 'is an empty file.'
+            return ''
+        f.seek(0)
+        root.read(f)
+    return str(root.traverse(recursive=True, verbose=args.verbose))
 
 
-dnaStore = DNAStorage.DNAStorage()
-data = loadDNAFile(dnaStore, args.filename)
-storeData = dnaStore.dump()
+def process_single_file(filename):
+    dnaStore = DNAStorage.DNAStorage()
+    rootData = loadDNAFile(dnaStore, filename)
 
-print 'Writing...', args.output
+    dnaStoreData = dnaStore.dump(verbose=args.verbose)
+    output = os.path.splitext(filename)[0] + '.pdna'
+    print 'Writing...', output
+    data = str(dnaStoreData + rootData)
+    if args.compress:
+        import zlib
+        data = zlib.compress(data)
+    header = 'PDNA\n{0}\n'.format(chr(1 if args.compress else 0))
+    with open(output, 'wb') as f:
+        f.write(header + data)
+    if args.verbose:
+        catalogCodeCount = 0
+        for root, codes in dnaStore.catalogCodes.items():
+            catalogCodeCount += len(codes)
+        print 'Catalog code count:', catalogCodeCount
+        print 'Texture count:', len(dnaStore.textures)
+        print 'Font count:', len(dnaStore.fonts)
+        print 'Node count:', len(dnaStore.nodes)
+        print 'Hood node count:', len(dnaStore.hoodNodes)
+        print 'Place node count:', len(dnaStore.placeNodes)
+        print 'Block number count:', len(dnaStore.blockNumbers)
+        print 'Block zone ID count:', len(dnaStore.blockZones)
+        print 'Block title count:', len(dnaStore.blockTitles)
+        print 'Block article count:', len(dnaStore.blockArticles)
+        print 'Block building type count:', len(dnaStore.blockBuildingTypes)
+        print 'DNASuitPoint count:', len(dnaStore.suitPoints)
+        print 'DNASuitEdge count:', len(dnaStore.suitEdges)
+    print 'Done processing %s.' % filename
 
-data = struct.pack('<I', len(storeData)) + storeData + data
+for filename in args.filenames:
+    filelist = []
+    if '*' in filename or '?' in filename:
+        filelist = glob.glob(filename)
+    else:
+        filelist.append(filename)
 
-if args.compress:
-    import zlib
-    data = zlib.compress(data)
-
-header = 'PDNA\n{0}\n'.format(chr(1 if args.compress else 0))
-
-with open(args.output, 'wb') as f:
-    f.write(header + data)
-
-if args.verbose:
-    catalogCodeCount = 0
-    for root, codes in dnaStore.catalogCodes.items():
-        catalogCodeCount += len(codes)
-    print 'Catalog code count:', catalogCodeCount
-    print 'Texture count:', len(dnaStore.textures)
-    print 'Font count:', len(dnaStore.fonts)
-    print 'Node count:', len(dnaStore.nodes)
-    print 'Hood node count:', len(dnaStore.hoodNodes)
-    print 'Place node count:', len(dnaStore.placeNodes)
-    print 'DNAGroup count:', len(dnaStore.DNAGroups)
-    print 'DNANode count:', len(dnaStore.DNANodes)
-    print 'DNAVisGroup count:', len(dnaStore.DNAVisGroups)
-    print 'Block number count:', len(dnaStore.blockNumbers)
-    print 'Block zone ID count:', len(dnaStore.blockZones)
-    print 'Block title count:', len(dnaStore.blockTitles)
-    print 'Block article count:', len(dnaStore.blockArticles)
-    print 'Block building type count:', len(dnaStore.blockBuildingTypes)
-    print 'DNASuitPoint count:', len(dnaStore.suitPoints)
-    print 'DNASuitEdge count:', len(dnaStore.suitEdges)
-    print 'DNABattleCell count:', len(dnaStore.battleCells)
+    for file in filelist:
+        process_single_file(file)
 
 print 'Done.'
---
+++ dubrari/tools/compiler/compileAll.bat
@@ -0,0 +1,13 @@
+"..\..\..\Panda3D-1.10.0\python\ppython.exe" compile.py ..\..\..\resources\phase_3.5\dna\*.dna
+"..\..\..\Panda3D-1.10.0\python\ppython.exe" compile.py ..\..\..\resources\phase_4\dna\*.dna
+"..\..\..\Panda3D-1.10.0\python\ppython.exe" compile.py ..\..\..\resources\phase_5\dna\*.dna
+"..\..\..\Panda3D-1.10.0\python\ppython.exe" compile.py ..\..\..\resources\phase_5.5\dna\*.dna
+"..\..\..\Panda3D-1.10.0\python\ppython.exe" compile.py ..\..\..\resources\phase_6\dna\*.dna
+"..\..\..\Panda3D-1.10.0\python\ppython.exe" compile.py ..\..\..\resources\phase_7\dna\*.dna
+"..\..\..\Panda3D-1.10.0\python\ppython.exe" compile.py ..\..\..\resources\phase_8\dna\*.dna
+"..\..\..\Panda3D-1.10.0\python\ppython.exe" compile.py ..\..\..\resources\phase_9\dna\*.dna
+"..\..\..\Panda3D-1.10.0\python\ppython.exe" compile.py ..\..\..\resources\phase_10\dna\*.dna
+"..\..\..\Panda3D-1.10.0\python\ppython.exe" compile.py ..\..\..\resources\phase_11\dna\*.dna
+"..\..\..\Panda3D-1.10.0\python\ppython.exe" compile.py ..\..\..\resources\phase_12\dna\*.dna
+"..\..\..\Panda3D-1.10.0\python\ppython.exe" compile.py ..\..\..\resources\phase_13\dna\*.dna
+pause
