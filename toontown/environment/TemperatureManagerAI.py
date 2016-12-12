aaffc13dcb808fe79618ea28597b47befe90bd96
AnythingTechPro <anythingtechpro@gmail.com>
2016-12-12 13:10:21 -0500

    Store and randomly set a temperature based on low,high average points

--- toontown/environment/TemperatureManagerAI.py
+++ toontown/environment/TemperatureManagerAI.py
@@ -1,5 +1,7 @@
 from toontown.toonbase import ToontownGlobals
 from toontown.environment import TemperatureGlobals
+import random
+import math
 
 class TemperatureManagerAI:
 
@@ -7,17 +9,22 @@ class TemperatureManagerAI:
         self.air = air
         self._temperatures = {}
     
-    def update(self, zoneId):
-        pass
+    def calculateRandom(self, high, low):
+        return random.randint(low, high) # todo: kinda cheep way of randomly setting starting tempearture
+    
+    def update(self, task, zoneId):
+        # todo, update temperature based on time of day.
+        return task.again
     
     def loadHood(self, zoneId):
         if zoneId not in ToontownGlobals.Hoods:
             return
         
-        self._temperatures[zoneId] = 0.0
+        self._temperatures[zoneId] = self.calculateRandom(*TemperatureGlobals.hood2average[
+            zoneId])
     
     def unloadHood(self, zoneId):
-        if zoneId not in self._temperatures:
+        if zoneId not in self._temperatures.keys():
             return
         
         del self._temperatures[zoneId]
