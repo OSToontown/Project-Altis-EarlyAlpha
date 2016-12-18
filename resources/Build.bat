3a76eb1dcfdefbca9b5f13ab3a4778f83b8cb1e5
CodeAnGo <luke@codeango.com>
2016-12-18 22:10:21 +0000

    Fuckerduckerdoo

--- resources/Build.bat
+++ resources/Build.bat
@@ -1,17 +1,17 @@
 @echo off
-..\Panda3D-1.9.0\bin\multify -cf phase_3.mf phase_3
-..\Panda3D-1.9.0\bin\multify -cf phase_3.5.mf phase_3.5
-..\Panda3D-1.9.0\bin\multify -cf phase_4.mf phase_4
-..\Panda3D-1.9.0\bin\multify -cf phase_5.mf phase_5
-..\Panda3D-1.9.0\bin\multify -cf phase_5.5.mf phase_5.5
-..\Panda3D-1.9.0\bin\multify -cf phase_6.mf phase_6
-..\Panda3D-1.9.0\bin\multify -cf phase_7.mf phase_7
-..\Panda3D-1.9.0\bin\multify -cf phase_8.mf phase_8
-..\Panda3D-1.9.0\bin\multify -cf phase_9.mf phase_9
-..\Panda3D-1.9.0\bin\multify -cf phase_10.mf phase_10
-..\Panda3D-1.9.0\bin\multify -cf phase_11.mf phase_11
-..\Panda3D-1.9.0\bin\multify -cf phase_12.mf phase_12
-..\Panda3D-1.9.0\bin\multify -cf phase_13.mf phase_13
+..\Panda3D-1.10.0\bin\multify -cf phase_3.mf phase_3
+..\Panda3D-1.10.0\bin\multify -cf phase_3.5.mf phase_3.5
+..\Panda3D-1.10.0\bin\multify -cf phase_4.mf phase_4
+..\Panda3D-1.10.0\bin\multify -cf phase_5.mf phase_5
+..\Panda3D-1.10.0\bin\multify -cf phase_5.5.mf phase_5.5
+..\Panda3D-1.10.0\bin\multify -cf phase_6.mf phase_6
+..\Panda3D-1.10.0\bin\multify -cf phase_7.mf phase_7
+..\Panda3D-1.10.0\bin\multify -cf phase_8.mf phase_8
+..\Panda3D-1.10.0\bin\multify -cf phase_9.mf phase_9
+..\Panda3D-1.10.0\bin\multify -cf phase_10.mf phase_10
+..\Panda3D-1.10.0\bin\multify -cf phase_11.mf phase_11
+..\Panda3D-1.10.0\bin\multify -cf phase_12.mf phase_12
+..\Panda3D-1.10.0\bin\multify -cf phase_13.mf phase_13
 echo BUILT
 echo DO NOT CHECKOUT THE MFS
 pause
