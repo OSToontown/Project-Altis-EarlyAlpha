c87507f390238ffd90708939be0d251f20223750
AnythingTechPro <anythingtechpro@gmail.com>
2016-12-20 00:55:12 -0500

    Added BanManagerUD

---
+++ toontown/uberdog/BanManagerUD.py
@@ -0,0 +1,38 @@
+import os
+import json
+
+class BanManagerUD:
+
+    def __init__(self, air):
+        self.air = air
+        
+        self.bansFilename = 'bannedPlayers.json'
+        self.bansFileData = {}
+    
+    def setup(self):
+        if not os.path.exists(self.bansFilename):
+            with open(self.bansFilename, 'w') as file:
+                file.write(json.dumps({})); file.close()
+        
+        with open(self.banFilename, 'rb') as file:
+            self.bansFileData = json.loads(file.read()); file.close()
+    
+    def banToon(self, cookie, reason):
+        if cookie in self.bansFileData.keys():
+            # uh wtf just happened?
+            return
+        
+        self.bansFileData[cookie] = reason
+        
+        # update the storage file data
+        with open(self.bansFilename, 'r+') as file:
+            file.write(json.dumps(self.bansFileData)); file.close()
+    
+    def getToonBanned(self, cookie):
+        if cookie in self.bandFileData.keys():
+            return True
+        
+        return False
+    
+    def getToonBanReason(self, cookie):
+        return self.banFileData[cookie]
